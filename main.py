efa1c0e462c336a785c5be556260f504ecf81982
roniwellington <roniwellington@gmail.com>
2025-05-20 20:55:54 -0300

    implementação do princípio aberto/fechado e  principio da subistituição de Liskov

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from cliente import Cliente
 from item import Item
+from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delivery import PedidoDelivery
+from pagamento.pagamento_cartao import PagamentoCartao
+from pagamento.pagamento_pix import PagamentoPIX
+from pagamento.pagamento_factory import PagamentoFactory
 
 cliente = Cliente("Roni", "Alura")
 item_um = Item("Pizza", 30.0)
 item_dois = Item("Refrigerante", 5.0)
-print(f"Item: {item_um.nome}, Preço: {item_um.preco}")
+itens = [item_um, item_dois]
+
+taxa_entrega = 10.0
+pedido_retirada = PedidoRetirada(cliente, itens)
+pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+
+
+valor_pedido = pedido_delivery.calcular_total()
+# pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+# pagamento_pix = PagamentoPIX()
+# pagamento_pix.processar(valor_pedido)
+tipo_pagamento = "pix"
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
+pagamento.processar(valor_pedido)
---
+++ pedido/pedido.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class Pedido(ABC):
+    def __init__(self, cliente, itens):
+        self.cliente = cliente
+        self.itens = itens
+        
+        
+    @abstractmethod    
+    def calcular_total(self):
+        pass
